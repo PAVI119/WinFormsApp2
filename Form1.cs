7532e664b1e9d592433c171b36cd2772a564e928
viZkaHD <pauvizcainoluna@gmail.com>
2023-02-26 20:46:55 +0100

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox2_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox3_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+    }
+}
